90fb7b01ef69f2e0d51450b67837c18fe52219dc
bakakak <56017719+bakakak@users.noreply.github.com>
2024-01-24 10:29:00 +0600

    Add files via upload

---
+++ Lab3/Classes.py
@@ -0,0 +1,17 @@
+# ex 1
+class MyClass:
+  x = 5
+# ex 2
+class MyClass:
+  x = 5
+p1 = MyClass()
+# ex 3
+class MyClass:
+  x = 5
+p1 = MyClass()
+print(p1.x)
+# ex 4
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
---
+++ Lab3/Modules.py
@@ -0,0 +1,9 @@
+# ex 1
+import mymodule
+# ex 2
+import mymodule as mx
+# ex 3
+import mymodule
+print(dir(mymodule))
+# ex 4
+from mymodule import person1
